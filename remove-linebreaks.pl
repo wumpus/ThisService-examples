563de7eecf648e1554e6806fcdbf3d4ece7a845c
Greg Lindahl <greg@blekko.com>
2013-06-05 14:13:27 -0700

    Who knew linebreaks were so complicated?

--- remove-linebreaks.pl
+++ remove-linebreaks.pl
@@ -1,9 +1,18 @@
-#!/usr/bin/perl -CIO
+#!/usr/bin/env perl -CIO
 
-while ( <> )
-{
-    s/\n/ /g;
-    s/\t/ /g;
-    s/  $/ /;
-    print;
-}
+my $text = do { local $/; <> };
+
+# make multiple whitespace single space
+# (also takes care of tabs)
+$text =~ s/[ \t]+/ /g;
+
+# turn single \n's into spaces without killing paragraphs
+$text =~ s/ \n/\n/g;
+$text =~ s/\n /\n/g;
+$text =~ s/\n/ /g;
+$text =~ s/  /\n\n/g;
+
+# delete final space
+$text =~ s/ $//;
+
+print $text;
